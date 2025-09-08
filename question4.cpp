649abb897080cbd3602bb5100b96abac36061a8e
mc250041009-cmyk <mc250041009@iiti.ac.in>
2025-09-08 16:49:14 +0530

    Update question4.cpp

--- question4.cpp
+++ question4.cpp
@@ -5,8 +5,11 @@
 using namespace std;
 
 // TODO: Define TaylorTerm struct
-// struct TaylorTerm {
-// };
+struct TaylorTerm {
+int order;
+double coeff;
+double value;
+};
 
 void print_taylor_term(int index, int order, double coeff, double value) {
     cout << "Term[" << index << "]: "
@@ -40,13 +43,27 @@ int main(int argc, char* argv[]) {
     input >> x >> terms;
 
     // TODO: Create an array of TaylorTerm
-    // TaylorTerm series[terms];
+    TaylorTerm series[terms];
 
     double approximation = 0.0;
 
     // TODO: Compute each Taylor term and store in array
-    // for (int k = 0; k < terms; k++) {
-    // }
+    for (int k = 0; k < terms; k++) {
+     int order = 2 * k + 1;
+     double coeff = pow(-1, k) / tgamma(order + 1);
+     double value = coeff * pow(x, order);
+     
+     series[k].order = order;
+     series[k].coeff = coeff;
+     series[k].value = value;
+     
+     approximation += value;
+    }
+    for (int i = 0; i < terms; i++) {
+        print_taylor_term(i, series[i].order, series[i].coeff, series[i].value);
+        }
+        
+        print_taylor_result("sin", x, terms, approximation);
 
     // TODO: Print final approximation
 
